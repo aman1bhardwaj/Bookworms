47c55ee9b8be15066fe7a7ceca6ff478c0b37fd6
aman1bhardwaj <84137675+aman1bhardwaj@users.noreply.github.com>
2021-09-06 00:06:03 +0530

    further improved

--- Bookworms/src/main/java/com/bookworm/Controller/ProductController.java
+++ Bookworms/src/main/java/com/bookworm/Controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,7 +42,7 @@ public class ProductController {
 		return this.prodserv.getallProduct();
 	}
 	
-	@PostMapping("/updateProduct/{id}")
+	@PutMapping("/updateProduct/{id}")
 	public Product updateProduct(Product prod) {
 		return this.prodserv.updateProduct(prod);
 	}
